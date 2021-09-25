3f6397f0ca35e812306c8774bea053dbf6145245
Eason Lin <uu9924079@gmail.com>
2021-09-25 21:50:56 +0800

    [功能] Switch bar 切換樣式

---
+++ calculator-app-main/assets/js/main.js
@@ -0,0 +1,20 @@
+const switchBar = document.querySelector(".switch-bar");
+const THEME_HREF_PREFIX = "assets/css/theme-";
+
+switchBar.addEventListener("click", changeTheme);
+
+function changeTheme() {
+  const theme = document.querySelector("link#theme");
+  if (!theme) {
+    console.error("[changeTheme Error] theme not found");
+    return false;
+  }
+  const themeNumber = Number(theme.href.replace(".css", "").slice(-1));
+  console.log(themeNumber);
+  if (Number.isNaN(themeNumber) || themeNumber > 3 || themeNumber < 1) {
+    console.error("[changeTheme Error] theme number is not valid");
+    return false;
+  }
+  const newThemeNumber = themeNumber === 3 ? 1 : themeNumber + 1;
+  theme.href = `${THEME_HREF_PREFIX}${newThemeNumber}.css`;
+}
