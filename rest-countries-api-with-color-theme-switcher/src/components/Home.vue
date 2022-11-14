2c6b0ca7dacb22debe44730e8f67a4d0b7d13c90
EasonLin0716 <uu9924079@gmail.com>
2022-11-14 19:43:11 +0800

    chore: add input and select

--- rest-countries-api-with-color-theme-switcher/src/components/Home.vue
+++ rest-countries-api-with-color-theme-switcher/src/components/Home.vue
@@ -2,15 +2,26 @@
 import { ref } from 'vue'
 import flagApi from '../api/flagApi'
 
-const flagData = ref([])
+const allFlagData = ref([])
+const regionData = ref([])
+const searchInput = ref('')
+const selectInput = ref('')
 flagApi.getFlags().then(data => {
-  flagData.value = data
+  allFlagData.value = data
+  data.forEach((item) => {
+    if (!regionData.value.includes(item.region)) regionData.value.push(item.region)
+  })
 })
 </script>
 
 <template>
+  <input type="text" v-model="searchInput" />
+  <select v-model="selectInput">
+    <option value="">Filter by Region</option>
+    <option v-for="region in regionData" :key="region" :value="region">{{ region }}</option>
+  </select>
   <ul>
-    <li v-for="(flag, index) in flagData" :key="index">{{ flag.name.common }}</li>
+    <li v-for="(flag, index) in allFlagData" :key="index">{{ flag.name.common }}</li>
   </ul>
 </template>
 
