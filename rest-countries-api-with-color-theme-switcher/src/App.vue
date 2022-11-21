06fedc798460af97774920abadfa671455dd5ad8
EasonLin0716 <uu9924079@gmail.com>
2022-11-21 21:29:43 +0800

    chore: PC index page

--- rest-countries-api-with-color-theme-switcher/src/App.vue
+++ rest-countries-api-with-color-theme-switcher/src/App.vue
@@ -1,13 +1,14 @@
 <template>
   <div id="app">
-    <header>
-      <h1>Where in the world?</h1>
-      <div>Dark Mode</div>
+    <header class="bg-white shadow-md">
+      <div class="max-w-7xl mx-auto p-6 flex justify-between items-center">
+        <h1 class="text-2xl font-extrabold">Where in the world?</h1>
+        <div class="flex items-center">
+          <i class="fa-regular fa-moon"></i>
+          <span class="pl-2">Dark Mode</span>
+        </div>
+      </div>
     </header>
-    <p>
-      <router-link to="/">Go to Home</router-link>
-      <router-link to="/about">Go to About</router-link>
-    </p>
     <router-view></router-view>
   </div>
 </template>
--- rest-countries-api-with-color-theme-switcher/src/components/Home.vue
+++ rest-countries-api-with-color-theme-switcher/src/components/Home.vue
@@ -24,14 +24,37 @@ const updateSearchInputInDelay = debounce((event) => {
 </script>
 
 <template>
-  <input type="text" @input="updateSearchInputInDelay" />
-  <select v-model="selectInput">
-    <option value="">Filter by Region</option>
-    <option v-for="region in regionData" :key="region" :value="region">{{ region }}</option>
-  </select>
-  <ul>
-    <li v-for="(flag, index) in filteredFlagData" :key="index">{{ flag.name.common }}</li>
-  </ul>
+  <div class="max-w-7xl mx-auto py-8">
+    <div class="flex justify-between mb-8">
+      <div class="px-7 py-4 bg-white shadow rounded">
+        <i class="fa-solid fa-magnifying-glass text-[var(--light-mode-dark-gray)]"></i>
+        <input class="pl-7 placeholder:text-[var(--light-mode-dark-gray)] focus:outline-none w-96" type="text" placeholder="Search for a country..." @input="updateSearchInputInDelay" />
+      </div>
+      <select v-model="selectInput" class="home-select">
+        <option value="">Filter by Region</option>
+        <option v-for="region in regionData" :key="region" :value="region">{{ region }}</option>
+      </select>
+    </div>
+    <section class="grid grid-cols-4 gap-16">
+      <router-link v-for="(flag, index) in filteredFlagData" :key="index" :to="'/' + flag.name.common" class="bg-white rounded-lg shadow-md overflow-hidden">
+        <img :src="flag.flags.png" class="w-full aspect-video object-cover" alt="">
+        <div class="p-6">
+          <h4 class="font-bold mb-2">{{ flag.name.common }}</h4>
+          <ul class="flex flex-col gap-1">
+            <li class="flex text-xs">
+              <p class="font-bold mr-1">Population:</p><span>{{ flag.population }}</span>
+            </li>
+            <li class="flex text-xs">
+              <p class="font-bold mr-1">Region:</p><span>{{ flag.region }}</span>
+            </li>
+            <li v-if="flag.capital" class="flex text-xs">
+              <p class="font-bold mr-1">Capital:</p><span>{{ flag.capital[0] }}</span>
+            </li>
+          </ul>
+        </div>
+      </router-link>
+    </section>
+  </div>
 </template>
 
 <style scoped>
