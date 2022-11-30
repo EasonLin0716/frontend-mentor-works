27f25b49969ca07e62c919d53dd5680c724ff6c3
EasonLin0716 <uu9924079@gmail.com>
2022-11-30 22:55:43 +0800

    chore: split AppHeader

--- rest-countries-api-with-color-theme-switcher/src/App.vue
+++ rest-countries-api-with-color-theme-switcher/src/App.vue
@@ -1,14 +1,10 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue'
+</script>
+
 <template>
   <div id="app">
-    <header class="bg-white shadow-md">
-      <div class="max-w-7xl mx-auto p-6 flex justify-between items-center">
-        <h1 class="text-2xl font-extrabold">Where in the world?</h1>
-        <div class="flex items-center">
-          <i class="fa-regular fa-moon"></i>
-          <span class="pl-2">Dark Mode</span>
-        </div>
-      </div>
-    </header>
+    <AppHeader />
     <router-view></router-view>
   </div>
 </template>
---
+++ rest-countries-api-with-color-theme-switcher/src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<template>
+    <header class="bg-white shadow-md">
+      <div class="max-w-7xl mx-auto p-6 flex justify-between items-center">
+        <h1 class="text-2xl font-extrabold">Where in the world?</h1>
+        <div class="flex items-center">
+          <i class="fa-regular fa-moon"></i>
+          <span class="pl-2">Dark Mode</span>
+        </div>
+      </div>
+    </header>
+</template>
