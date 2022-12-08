6ad86e445f127481fae116dde28e3fd8902d284d
EasonLin0716 <uu9924079@gmail.com>
2022-12-08 22:32:06 +0800

    chore: battle card(15%)

---
+++ rock-paper-scissors/src/components/MoveCard.vue
@@ -0,0 +1,16 @@
+<script setup>
+defineProps({
+    svgLink: {
+        type: String,
+        required: true,
+    },
+})
+</script>
+
+<template>
+    <div class="w-32 h-32 flex justify-center items-center rounded-1/2 bg-orange-500">
+        <div class="w-24 h-24 rounded-1/2 flex justify-center items-center bg-white">
+            <img :src="'/images/icon-' + svgLink + '.svg'" :alt="svgLink" class="w-14 h-auto">
+        </div>
+    </div>
+</template>
