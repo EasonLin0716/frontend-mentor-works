c7b769be23714baa9a572929c5d4f54784640579
EasonLin0716 <uu9924079@gmail.com>
2022-12-26 22:08:58 +0800

    chore: css mobile

--- rock-paper-scissors/src/components/GameRules.vue
+++ rock-paper-scissors/src/components/GameRules.vue
@@ -5,17 +5,17 @@ const isDisplay = ref(false)
 
 <template>
     <button 
-        class="absolute bottom-8 right-8 text-white uppercase tracking-widest text-lg py-2 px-10 rounded-lg border-1 border-white"
+        class="pc:right-8 pc:transform-none absolute bottom-8 right-1/2 translate-x-1/2 transform text-white uppercase tracking-widest text-lg py-2 px-10 rounded-lg border-1 border-white"
         @click="isDisplay = true"
     >rules</button>
     <Transition name="fade">
         <div v-if="isDisplay" class="absolute w-full h-full top-0 left-0 bg-opacity-50 bg-black z-20">
-            <div class="fixed top-1/2 left-1/2  bg-white pt-7 px-10 pb-10 rounded-lg -translate-x-1/2 -translate-y-1/2 transform" >
-                <div class="flex justify-between items-baseline">
+            <div class="pc:top-1/2 pc:left-1/2 pc:rounded-lg pc:-translate-x-1/2 pc:-translate-y-1/2 pc:transform pc:w-auto pc:h-auto fixed  bg-white pt-7 px-10 pb-10 rounded-none w-full h-full" >
+                <div class="pc:pt-0 pc:justify-between flex justify-center items-baseline pt-20">
                     <h4 class="uppercase text-3xl font-bold mb-4 text-[var(--dark-text)]">rules</h4>
-                    <button class="text-4xl text-gray-400 font-bold" @click="isDisplay = false">&times;</button>
+                    <button class="pc:static pc:transform-none absolute bottom-20 left-1/2 -translate-x-1/2 transform text-4xl text-gray-400 font-bold" @click="isDisplay = false">&times;</button>
                 </div>
-                <img src="/images/image-rules-bonus.svg" alt="">
+                <img class="pc:mx-0 pc:static pc:transform-none mx-auto absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform" src="/images/image-rules-bonus.svg" alt="">
             </div>
         </div>
     </Transition>
--- rock-paper-scissors/src/components/ScoreBoard.vue
+++ rock-paper-scissors/src/components/ScoreBoard.vue
@@ -12,13 +12,13 @@ const props = defineProps({
 </script>
 
 <template>
-    <div class="flex justify-between mb-10 pc:px-8 pc:py-6 px-6 py-4 border-solid border-4 border-[var(--header-outline)] rounded-2xl pc:mx-auto mx-4 pc:max-w-[768px] max-w-[468px]">
+    <div class="pc:px-8 pc:py-6 pc:mx-auto mx-4 pc:max-w-[768px] flex justify-between mb-10 px-6 py-4 border-solid border-4 border-[var(--header-outline)] rounded-2xl max-w-[468px]">
         <ul>
-            <li v-for="card in cardList" :index="card" class="text-white uppercase font-bold pc:text-2xl text-lg leading-none ">{{ card }}</li>
+            <li v-for="card in cardList" :index="card" class="pc:text-2xl pc:leading-none text-white uppercase font-bold text-lg leading-none">{{ card }}</li>
         </ul>
-        <div class="bg-white rounded-xl pc:px-12 px-5 py-4 ">
-            <p class="text-[var(--score-text)] uppercase pc:text-lg text-base tracking-widest text-center">score</p>
-            <span class="text-[var(--dark-text)] pc:text-6xl text-4xl font-bold text-center block">{{ score }}</span>
+        <div class="pc:px-12 bg-white rounded-xl px-5 py-4 ">
+            <p class="pc:text-lg text-[var(--score-text)] uppercase text-base tracking-widest text-center">score</p>
+            <span class="pc:text-6xl text-[var(--dark-text)] text-4xl font-bold text-center block">{{ score }}</span>
         </div>
     </div> 
 </template>
