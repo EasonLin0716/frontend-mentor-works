919ee0ea58caeb23719e53bdd6aaff859e41f86a
Eason Lin <uu9924079@gmail.com>
2024-07-02 19:20:21 +0800

    chore: change string to data object

--- connect-four-game/src/components/specific/GameBoard.tsx
+++ connect-four-game/src/components/specific/GameBoard.tsx
@@ -5,22 +5,30 @@ import { GameTurn, GamePawn, PutButton, GameMarker } from '.';
 const GameBoard: React.FC = () => {
     const X = 7;
     const Y = 6;
-    const initialBoard = Array.from({ length: X }, () => Array.from({ length: Y }, () => ''));
+    const initialBoard = Array.from({ length: X }, () => Array.from({ length: Y }, () => ({
+        className: '',
+        isPlayer1: false,
+        isSet: false,
+    })));
     const [board, setBoard] = useState(initialBoard);
     const btnLength = X * Y;
 
     const clickHandler = (i: number): void => {
         const putX = i % 7;
         let putY = 0;
-        if (board[putX][putY]) {
+        if (board[putX][putY]['isSet']) {
             console.log('full');
             return;
         }
-        while (board[putX][putY + 1] !== undefined && board[putX][putY + 1] === '') {
+        while (board[putX][putY + 1] !== undefined && board[putX][putY + 1]['isSet'] === false) {
             putY++;
         }
         const newBoard = [...board];
-        newBoard[putX][putY] = `x-${putX} y-${putY}`;
+        newBoard[putX][putY] = {
+            className: `x-${putX} y-${putY}`,
+            isPlayer1: true,
+            isSet: true,
+        }
         setBoard(newBoard);
     }
 
@@ -33,8 +41,8 @@ const GameBoard: React.FC = () => {
             <div className={styles.bgBack}></div>
             <div className={styles.pawns}>
                 {board.map((col, x) =>
-                    col.map((c, y) =>
-                        board[x][y] && <GamePawn key={`${x}-${y}`} className={`${c} ${styles.pawn}`} />
+                    col.map((p, y) =>
+                        board[x][y]['isSet'] && <GamePawn key={`${x}-${y}`} className={`${p.className} ${styles.pawn}`} />
                     )
                 )}
             </div>
