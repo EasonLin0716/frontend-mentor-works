44c8288495f3ff69002193498ebe61e0f388d2d6
Eason <uu9924079@gmail.com>
2024-07-19 09:49:12 +0800

    chore: header icons

---
+++ dictionary-web-app/src/lib/Header.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import logo from '../assets/logo.svg';
+  import iconArrowDown from '../assets/icon-arrow-down.svg';
+  import iconMoon from '../assets/icon-moon.svg';
+</script>
+
+<div class="header">
+  <div class="left">
+    <img src={logo} alt="logo" width="32" height="36.5" />
+  </div>
+  <div class="right">
+    <button class="font-toggler">
+      <img src={iconArrowDown} alt="arrow down icon" />
+    </button>
+    <button class="dark-mode-toggler">
+      <img src={iconMoon} alt="moon icon" />
+    </button>
+  </div>
+</div>
+
+<style></style>
---
+++ dictionary-web-app/src/lib/SearchInput.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import iconSearch from '../assets/icon-search.svg';
+</script>
+
+<div class="search-input">
+  <input type="text" placeholder="Search for a word" />
+  <button>
+    <img src={iconSearch} alt="search icon" />
+  </button>
+</div>
+
+<style></style>
